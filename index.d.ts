61be37cd1301b68581b76fc58c8f9dd36d9d55f7
tpluscode <tpluscode@users.noreply.github.com>
2020-10-26 12:10:27 +0100

    refactor: remove lines agreed by PR reviewers

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,3 @@
-// Type definitions for the RDFJS specification 4.0
-// Project: https://github.com/rdfjs/representation-task-force
-// Definitions by: Ruben Taelman <https://github.com/rubensworks>
-//                 Laurens Rietveld <https://github.com/LaurensRietveld>
-//                 Tomasz Pluskiewicz <https://github.com/tpluscode>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
-
 export * from './data-model';
 export * from './stream';
 export * from './dataset';
